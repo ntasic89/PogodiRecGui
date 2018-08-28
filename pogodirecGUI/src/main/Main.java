f39d9a2deb7be810303fc0d6b599273e69a13a69
nemanja <nemanja@DESKTOP-T6OODPG>
2018-08-28 12:19:07 +0200

    FIRST COMMIT

---
+++ pogodirecGUI/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.EventQueue;
+
+import forms.frmMain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmMain window = new frmMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
